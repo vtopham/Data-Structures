7093f145b051c2bbaea3405fcd800f1d4d907774
Victoria  Topham <topham.victoria@gmail.com>
2020-06-08 14:27:54 -0700

    implemented a queue using arrays

--- queue/queue.py
+++ queue/queue.py
@@ -16,13 +16,16 @@ Stretch: What if you could only use instances of your Stack class to implement t
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.storage.append(value)
+        self.size = len(self.storage)
 
     def dequeue(self):
-        pass
+        if self.size > 0:
+            self.size = len(self.storage) - 1
+            return self.storage.pop(0)
